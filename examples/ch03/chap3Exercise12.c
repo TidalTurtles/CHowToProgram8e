40b97fd8fe10a1876c6f25db30576f66002671b2
Noah Holt <tidalturtles@gmail.com>
2021-09-05 17:20:34 -0600

    initial homework A02

---
+++ examples/ch03/chap3Exercise12.c
@@ -0,0 +1,27 @@
+//
+//  chap3Exercise12.c
+//  CS2060ClassWork
+//
+//  Created by noah holt on 9/5/21.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    unsigned int x = 1;
+    unsigned int total = 0;
+    unsigned int y;
+    
+    while (x <= 10) {
+        
+        y = x * x;
+        printf("%d\n", y);
+        total += y;
+        ++x;
+        
+    }
+    
+    printf("Total is %d\n", total);
+    
+}
