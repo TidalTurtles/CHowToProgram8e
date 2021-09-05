effa405084efe05025835cbad1c487ef6d9a4dd0
Noah Holt <tidalturtles@gmail.com>
2021-09-05 17:13:45 -0600

    original

---
+++ examples/ch02/charInput.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Explore Character Input and Output
+Improve the quality of the code
+Improve the output  to understand what is being printed
+Put comments in the code to explain what is happening
+
+*/
+
+
+int main() {
+	
+	//What is happening below?
+	char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    printf ("Value:  %c\n",c);
+    printf ("Value:  %d\n",c);
+    int num;
+	printf("Enter value from 0 to 255: ");
+	scanf("%d",&num); 	
+	printf("Value: %c\n",num);
+	printf("Value:  %d\n",num);
+	 
+}
